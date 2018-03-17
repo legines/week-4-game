818b62202ba64ead85e994c78dfb7c56f3005236
unknown <legines@gmail.com>
2018-03-17 07:58:43 -0400

    finished styling. RNG works

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  // Player variables set to 0
+  var score = 0;
+  var wins = 0;
+  var loss = 0;
+  // Generates random number to guess
+  var Random=Math.floor(Math.random()*102+19)
+  
+  // Display random number
+  $("#ranNum").text(Random);
+  
+  // Assign random number to crystals
+
+  // Crystal click events
+
+  // Win/Lose Counters
+
+  // Restart game
+
+
+ 
+}); 
